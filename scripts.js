d1bf9b4109dbcc7dbffbf915c2eae923467b0956
Ksoler23 <123099353+Ksoler23@users.noreply.github.com>
2024-07-23 23:05:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    gsap.from("#intro", { duration: 13, y: -50, opacity: 0 });
+    gsap.from("#intro", { duration: 13, y: -100, opacity: 10 });
 });
